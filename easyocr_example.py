84ed8dd7a711e56d871606bc4822d37963f835b6
hindami <hindami050294@gmail.com>
2025-07-30 16:53:52 +0700

    add config with openCV

--- easyocr_example.py
+++ easyocr_example.py
@@ -1,11 +1,33 @@
 import easyocr
 
-reader = easyocr.Reader(
-    ["ch_sim", "en"]
-)  # this needs to run only once to load the model into memory
-result = reader.readtext(
-    "./samples/handwrite-anything-for-you-in-neat-clear-handwriting-0a3b.webp"
+import cv2
+
+
+# Basic
+
+# reader = easyocr.Reader(["en"])
+# result = reader.readtext("./samples/split_1.png", detail=3)
+
+# for _, text, _ in result:
+#     print(text)
+
+# Using OpenCV
+contrast = 1.1
+brightness = -1
+
+pict = cv2.imread(
+    "./samples/hand-write-documents-letters-and-notes-in-beautiful-handwriting-styles.webp"
 )
+
+adjusted_image = cv2.convertScaleAbs(pict, alpha=contrast, beta=brightness)
+
+reader = easyocr.Reader(["en"])
+result = reader.readtext(adjusted_image)
 
 for _, text, _ in result:
     print(text)
+
+# cv2.imshow("Original Image", pict)
+# cv2.imshow("Adjusted Contrast", adjusted_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
